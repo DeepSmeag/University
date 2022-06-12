3fa7d5e9657d94a49f09253058ddc4171556d3a3
Cosmin <43291739+DeepSmeag@users.noreply.github.com>
2022-06-12 15:57:05 +0300

    added build

---
+++ An1/Sem2/OOP/test/build/CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/source_autogen.dir/ParseCache.txt"
+  "source_autogen"
+  "CMakeFiles/source.dir/source_autogen/mocs_compilation.cpp.o"
+  "libsource.a"
+  "libsource.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
