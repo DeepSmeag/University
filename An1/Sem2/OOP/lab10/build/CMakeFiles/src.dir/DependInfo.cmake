f7b87bdd33b056df455e147392aa97a77914eef9
Cosmin <43291739+DeepSmeag@users.noreply.github.com>
2022-06-12 15:10:32 +0300

    added build folder

---
+++ An1/Sem2/OOP/lab10/build/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/domain/cart.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/domain/cart.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/domain/listOffers.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/domain/listOffers.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/domain/offer.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/domain/offer.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/domain/report.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/domain/report.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/errors/errors.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/errors/errors.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/gui.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/gui.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/guiModelView.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/guiModelView.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/main.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/main.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/repo/repo.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/repo/repo.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/service/service.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/service/service.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/src_autogen/mocs_compilation.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/src_autogen/mocs_compilation.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src/valid/valid.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/build/CMakeFiles/src.dir/valid/valid.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/lab10/src"
+  "src_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
