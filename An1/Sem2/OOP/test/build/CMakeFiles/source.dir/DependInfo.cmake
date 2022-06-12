3725a0aacfd685058c00ef7da5da3d660e71d51c
Cosmin <43291739+DeepSmeag@users.noreply.github.com>
2022-06-12 15:57:05 +0300

    added build

---
+++ An1/Sem2/OOP/test/build/CMakeFiles/source.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/source_autogen/mocs_compilation.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/source.dir/source_autogen/mocs_compilation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "source_autogen/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ An1/Sem2/OOP/test/build/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/domain.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/domain.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/gui.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/gui.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/main.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/main.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/repo.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/repo.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/service.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/service.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/src_autogen/mocs_compilation.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/src_autogen/mocs_compilation.cpp.o"
+  "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/tests.cpp" "/mnt/e/CodingAndDev/Projects/cC++/Facultate/OOP/test/build/CMakeFiles/src.dir/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "src_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
